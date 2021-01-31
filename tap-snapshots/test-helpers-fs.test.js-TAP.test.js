85f8005ab774224a43c8f50efe48bb6b0e680cc4
Mark Kvetny <m.kvetny@outlook.com>
2021-02-01 00:49:25 +0100

    add fs tests

--- tap-snapshots/test-helpers-fs.test.js-TAP.test.js
+++ tap-snapshots/test-helpers-fs.test.js-TAP.test.js
@@ -5,7 +5,42 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
-exports[`test/helpers/fs.test.js TAP UpdatePackageJSON > package.json error log 1`] = `
+exports[`test/helpers/fs.test.js TAP updateApp fallback > updateApp fallback 1`] = `
+import test from "test/js"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+
+`
+
+exports[`test/helpers/fs.test.js TAP updateApp fallback typescript > updateApp typescript fallback 1`] = `
+import test from "test/ts"
+
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pagesProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+`
+
+exports[`test/helpers/fs.test.js TAP updateApp update app.js > updateApp js 1`] = `
+import test from "test/js"
+
+const App = () => {};
+`
+
+exports[`test/helpers/fs.test.js TAP updateApp update app.tsx > updateApp tsx 1`] = `
+import test from "test/ts"
+
+const App = () => {};
+`
+
+exports[`test/helpers/fs.test.js TAP updatePackageJSON logs warning if there's no package.json > package.json error log 1`] = `
 Array [
   "\\u001b[1m\\u001b[31mwarning - no package.json found\\u001b[39m\\u001b[22m\\n",
 ]
--- test/helpers/fs.test.js
+++ test/helpers/fs.test.js
@@ -1,9 +1,11 @@
+// @ts-check
 import tap from "tap"
-import { readJSON, remove } from "fs-extra"
-import { updatePackageJSON } from "../../src/helpers/fs"
+import { outputFile, readFile, readJSON, remove, writeFile } from "fs-extra"
+import { updateApp, updatePackageJSON } from "../../src/helpers/fs"
 import { stderr } from "test-console"
 
 tap.beforeEach((done) => {
+	// @ts-ignore
 	const dir = tap.testdir({
 		"package.json": `{"name": "test-project"}`,
 	})
@@ -11,22 +13,68 @@ tap.beforeEach((done) => {
 	done()
 })
 
-tap.test("updates package.json", async (t) => {
-	await updatePackageJSON({ version: "1.0.0" })
-	const res = await readJSON("package.json")
+tap.test("updatePackageJSON", async (t) => {
+	t.test("updates package.json", async (t) => {
+		await updatePackageJSON({ version: "1.0.0" })
+		const res = await readJSON("package.json")
+		t.deepEqual(res, {
+			name: "test-project",
+			version: "1.0.0",
+		})
+		t.end()
+	})
 
-	t.deepEqual(res, {
-		name: "test-project",
-		version: "1.0.0",
+	t.test("logs warning if there's no package.json", async (t) => {
+		await remove("package.json")
+		const inspect = stderr.inspect()
+		await updatePackageJSON({ version: "1.0.0" })
+		inspect.restore()
+		t.matchSnapshot(inspect.output, "package.json error log")
+		t.end()
 	})
 	t.end()
 })
 
-tap.test("UpdatePackageJSON", async (t) => {
-	await remove("package.json")
-	const inspect = stderr.inspect()
-	await updatePackageJSON({ version: "1.0.0" })
-	inspect.restore()
-	t.matchSnapshot(inspect.output, "package.json error log")
+/** @type {(src: string, lang: "javascript" | "typescript") => string} */
+const updateCallback = (src, lang) => {
+	const stmt =
+		lang === "javascript"
+			? `import test from "test/js"`
+			: `import test from "test/ts"`
+	return `${stmt}\n\n${src}`
+}
+
+tap.test("updateApp", (t) => {
+	t.test("fallback", async (t) => {
+		await updateApp(updateCallback)
+		const res = await readFile("pages/_app.js", "utf-8")
+		t.matchSnapshot(res, "updateApp fallback")
+		t.end()
+	})
+
+	t.test("fallback typescript", async (t) => {
+		await writeFile("tsconfig.json", "{}")
+		await updateApp(updateCallback)
+		const res = await readFile("pages/_app.tsx", "utf-8")
+		t.matchSnapshot(res, "updateApp typescript fallback")
+		t.end()
+	})
+
+	t.test("update app.js", async (t) => {
+		await outputFile("pages/_app.js", "const App = () => {};")
+		await updateApp(updateCallback)
+		const res = await readFile("pages/_app.js", "utf-8")
+		t.matchSnapshot(res, "updateApp js")
+		t.end()
+	})
+
+	t.test("update app.tsx", async (t) => {
+		await outputFile("pages/_app.tsx", "const App = () => {};")
+		await updateApp(updateCallback)
+		const res = await readFile("pages/_app.tsx", "utf-8")
+		t.matchSnapshot(res, "updateApp tsx")
+		t.end()
+	})
+
 	t.end()
 })
