a066e397ded4405f7b1631d5f8e74529057145ac
Mark Kvetny <m.kvetny@outlook.com>
2021-02-01 00:41:21 +0100

    implement updateApp function

--- src/helpers/fs.js
+++ src/helpers/fs.js
@@ -1,13 +1,20 @@
 // @ts-check
-import { readFile, readJSON, writeFile, writeJSON } from "fs-extra"
+import {
+	outputFile,
+	pathExists,
+	readFile,
+	readJSON,
+	writeFile,
+	writeJSON,
+} from "fs-extra"
 import merge from "lodash.merge"
 import ansi from "ansi-colors"
+import { APP_JS, APP_TSX } from "./source"
 
 /** @typedef {Object<string, any>} JSON */
 
 /**
- * Merge the contents of a JSON file with
- * the provided object
+ * Merges the contents of a JSON file with the provided object.
  * @param {string} file
  * @param {JSON} object
  */
@@ -18,8 +25,7 @@ export async function updateJSON(file, object) {
 }
 
 /**
- * Merge contents of package.json with the
- * provided object.
+ * Merges contents of package.json with the provided object.
  * @param {JSON} dependencies
  */
 export function updatePackageJSON(dependencies) {
@@ -29,11 +35,42 @@ export function updatePackageJSON(dependencies) {
 }
 
 /**
- *
- * @param {string} file
- * @param {(src: string) => string} callback
+ * @callback updateCallback
+ * @param {string} src The content of the updated file.
+ * @returns {string} The updated content.
+ */
+
+/**
+ * Updates the content of a file. Takes a path to
+ * @param {string} file A path to the file to update.
+ * @param {updateCallback} callback
  */
 export async function updateFile(file, callback) {
 	const src = await readFile(file, "utf-8")
 	await writeFile(file, callback(src))
+}
+
+/**
+ * @callback updateWithLangCallback
+ * @param {string} src The content of the updated file.
+ * @param {"javascript" | "typescript"} language The language of the updated file.
+ */
+
+/**
+ * Updates the content of a Next.js _app.(js|tsx) file.
+ * Provides fallback content if no _app.(js|tsx) is found.
+ * @param {updateWithLangCallback} callback
+ */
+export async function updateApp(callback) {
+	if (await pathExists("pages/_app.js")) {
+		const src = await readFile("pages/_app.js", "utf-8")
+		await outputFile("pages/_app.js", callback(src, "javascript"))
+	} else if (await pathExists("pages/_app.tsx")) {
+		const src = await readFile("pages/_app.tsx", "utf-8")
+		await outputFile("pages/_app.tsx", callback(src, "typescript"))
+	} else if (await pathExists("tsconfig.json")) {
+		await outputFile("pages/_app.tsx", callback(APP_TSX, "typescript"))
+	} else {
+		await outputFile("pages/_app.js", callback(APP_JS, "javascript"))
+	}
 }
---
+++ src/helpers/source.js
@@ -0,0 +1,39 @@
+// prettier-ignore
+export const APP_JS =
+`function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+`
+
+// prettier-ignore
+export const APP_TSX =
+`import { AppProps } from "next/app";
+
+function MyApp({ Component, pagesProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;`
+
+// prettier-ignore
+export const DOCUMENT_JS =
+`import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+`;
