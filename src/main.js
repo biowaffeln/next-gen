fbf84d2b49fc909778e48112b1b33cf09be9c4eb
Mark Kvetny <m.kvetny@outlook.com>
2021-02-01 18:11:28 +0100

    cli setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
+// @ts-check
+import yargs from "yargs"
+import prompts from "prompts"
+
 export function cli() {
-	console.log("hello world")
+	yargs
+		.scriptName("next-gen")
+		.command("add [recipe]", "add a recipe to a Next.js project", () => {}, add)
+		.command("init", "create a new Next.js project", () => {}, init)
+		.demandCommand()
+		.recommendCommands()
+		.strict()
+		.help().argv
+}
+
+/**
+ * @param {{recipe?: string}} args
+ */
+async function add({ recipe }) {
+	if (!recipe) {
+		const response = await prompts({
+			name: "recipe",
+			message: "Select a recipe",
+			type: "autocomplete",
+			choices: [
+				{ title: "TypeScript", value: "typescript" },
+				{ title: "Sass", value: "sass" },
+			],
+		})
+		if (!response.recipe) {
+			process.exit(0)
+		}
+		recipe = response.recipe
+	}
+	// do something with recipe
+}
+
+function init() {
+	// TODO: implement
 }
