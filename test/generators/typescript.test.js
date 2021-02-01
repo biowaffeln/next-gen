4f504b0cf1fd05003ea540ed042f34e70deb9191
Mark Kvetny <m.kvetny@outlook.com>
2021-02-01 02:40:53 +0100

    test coverage

--- test/generators/typescript.test.js
+++ test/generators/typescript.test.js
@@ -1,9 +1,10 @@
 // @ts-check
 import tap from "tap"
-import { readFile, writeFile } from "fs-extra"
+import { createFile, pathExists, readFile, remove, writeFile } from "fs-extra"
 import { generatorTypeScript } from "../../src/generators/typescript"
 import { APP_JS, DOCUMENT_JS } from "../../src/helpers/source"
 import { DOCUMENT_JS_WITH_INITIAL_PROPS } from "../test-source"
+import { stderr } from "test-console"
 
 tap.beforeEach((done) => {
 	// @ts-ignore
@@ -31,5 +32,26 @@ tap.test("should add types to document", async (t) => {
 	await writeFile("pages/_document.js", DOCUMENT_JS_WITH_INITIAL_PROPS)
 	await generatorTypeScript()
 	t.matchSnapshot(await readFile("pages/_document.tsx", "utf-8"), "document")
+	t.end()
+})
+
+tap.test("should error", async (t) => {
+	const inspect = stderr.inspect()
+	await writeFile("pages/_document.js", "syntax error")
+	await writeFile("pages/_app.js", "syntax error")
+	await generatorTypeScript()
+	inspect.restore()
+	t.equals(inspect.output.length, 2)
+	t.end()
+})
+
+tap.test("should do nothing", async (t) => {
+	await remove("pages/_document.js")
+	await remove("pages/_app.js")
+	await createFile("tsconfig.json")
+	await generatorTypeScript()
+	t.false(await pathExists("pages/_document.tsx"))
+	t.false(await pathExists("pages/_app.tsx"))
+	t.equal(await readFile("tsconfig.json", "utf-8"), "")
 	t.end()
 })
