1b35d966efbdf61f2ffeb702746a222da4b7de64
Mark Kvetny <m.kvetny@outlook.com>
2021-02-05 14:35:11 +0100

    create script to check latest deps

---
+++ scripts/check-deps.js
@@ -0,0 +1,48 @@
+// @ts-check
+import fetch from "node-fetch";
+import semver from "semver";
+
+const depsMap = {
+	typescript: {
+		"@types/react": "^17.0.0",
+		"@types/node": "^14.14.22",
+		typescript: "^4.1.3",
+	},
+	preact: {
+		"@prefresh/next": "^1.3.0",
+		preact: "^10.5.5",
+		"preact-render-to-string": "^5.1.11",
+		"@preact/compat": "^0.0.4",
+		"preact-ssr-prepass": "^1.1.2",
+		"next-plugin-preact": "^3.0.3",
+	},
+	sass: {
+		sass: "^1.32.6",
+	},
+	tailwind: {
+		autoprefixer: "^10.2.1",
+		postcss: "^8.2.4",
+		tailwindcss: "^2.0.2",
+	},
+};
+
+function checkVersions() {
+	for (const [recipe, deps] of Object.entries(depsMap)) {
+		for (const [pkg, version] of Object.entries(deps)) {
+			fetch(`https://registry.npmjs.org/-/package/${pkg}/dist-tags`)
+				.then((res) => res.json())
+				.then((npm) => {
+					if (!semver.satisfies(npm.latest, version)) {
+						// prettier-ignore
+						console.log(
+`Package ${pkg} in ${recipe} is outdated:
+current version is ${version}, but latest is ${npm.latest}.
+`
+				);
+					}
+				});
+		}
+	}
+}
+
+checkVersions();
