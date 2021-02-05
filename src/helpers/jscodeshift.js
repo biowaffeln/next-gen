b10997932bb811cac0bff636161ef278ba5b537a
Mark Kvetny <m.kvetny@outlook.com>
2021-02-05 02:21:47 +0100

    implement `addRequire` function

--- src/helpers/jscodeshift.js
+++ src/helpers/jscodeshift.js
@@ -2,6 +2,12 @@
 import addImports from "jscodeshift-add-imports"
 
 /**
+ * Adds an import statement to a .js or .ts file.
+ * Merges imports if they exist, otherwise it appends
+ * the import after the existing ones.
+ *
+ * Similar to `jscodeshift-add-imports`, but fixes some
+ * formatting issues regarding blank lines.
  *
  * @param {import("jscodeshift")} j
  * @param {import("jscodeshift").Collection} root
@@ -27,7 +33,7 @@ export function addImport(j, root, statement) {
 		root.get().node.program.body.unshift(s)
 	}
 	// if there are imports but no merging is required,
-	// add the statement at the end of the imports (without blank lines before)
+	// add the statement at the end of the imports (without blank lines inbetween)
 	else if (!shouldMerge) {
 		imports.at(-1).replaceWith((p) =>
 			j.template.statements([
@@ -39,5 +45,37 @@ ${statement}`,
 	// otherwise, merge imports
 	else {
 		addImports(root, [s])
+	}
+}
+
+/**
+ * Adds a require statement to a .js file.
+ * Does not merge existing require calls.
+ *
+ * @param {import("jscodeshift")} j
+ * @param {import("jscodeshift").Collection} root
+ * @param {string} statement
+ */
+export function addRequire(j, root, statement) {
+	// find variable declarations that call require
+	const declarations = root.find(j.VariableDeclaration, {
+		declarations: {
+			0: { init: { callee: { name: "require" } } },
+		},
+	})
+
+	// if there aren't any, add require to top of the file
+	if (declarations.length === 0) {
+		const s = j.template.statement([statement + "\n\n"])
+		root.get().node.program.body.unshift(s)
+	}
+	// otherwise add it after last require
+	else {
+		declarations.at(-1).replaceWith((p) =>
+			j.template.statements([
+				`${j(p).toSource()}
+	${statement};`,
+			])
+		)
 	}
 }
--- test/helpers/codeshift.test.js
+++ test/helpers/codeshift.test.js
@@ -1,14 +1,13 @@
 // @ts-check
 import tap from "tap"
-import { addImport } from "../../src/helpers/jscodeshift"
+import { addImport, addRequire } from "../../src/helpers/jscodeshift"
 import { withParser } from "jscodeshift"
-const j = withParser("tsx")
 
 /**
  * A jscodeshift test case
  * @typedef {Object} TestCase
  * @property {string} code
- * @property {string} importString
+ * @property {string} statement
  * @property {string} expected
  */
 
@@ -19,10 +18,11 @@ const j = withParser("tsx")
  * @param {Test} t
  * @param {TestCase} testCase
  */
-const testCase = (name, t, { code, importString, expected }) => {
+const testCase = (name, t, { code, statement, expected }) => {
 	t.test(name, (t) => {
+		const j = withParser("tsx")
 		const root = j(code)
-		addImport(j, root, importString)
+		addImport(j, root, statement)
 		t.equal(root.toSource(), expected)
 		t.end()
 	})
@@ -31,12 +31,12 @@ const testCase = (name, t, { code, importString, expected }) => {
 tap.test("addImports", (t) => {
 	testCase("should add import to empty file", t, {
 		code: "",
-		importString: `import test from "test"`,
+		statement: `import test from "test"`,
 		expected: `import test from "test"`,
 	})
 	testCase("should add import, with proper formatting", t, {
 		code: "const value = test();",
-		importString: `import test from "test"`,
+		statement: `import test from "test"`,
 		expected: `import test from "test"
 
 const value = test();`,
@@ -44,13 +44,13 @@ const value = test();`,
 
 	testCase("should merge imports", t, {
 		code: `import React from "react";`,
-		importString: `import { Component } from "react";`,
+		statement: `import { Component } from "react";`,
 		expected: `import React, { Component } from "react";`,
 	})
 
 	testCase("should not merge imports", t, {
 		code: `import thing from "lib";`,
-		importString: `import otherThing from "other-lib";`,
+		statement: `import otherThing from "other-lib";`,
 		expected: `import thing from "lib";
 import otherThing from "other-lib";`,
 	})
@@ -59,7 +59,7 @@ import otherThing from "other-lib";`,
 		code: `import thing from "lib";
 
 const test = thing();`,
-		importString: `import otherThing from "other-lib";`,
+		statement: `import otherThing from "other-lib";`,
 		expected: `import thing from "lib";
 import otherThing from "other-lib";
 
@@ -68,13 +68,13 @@ const test = thing();`,
 
 	testCase("should add import without export", t, {
 		code: "",
-		importString: `import "../styles/index.css"`,
+		statement: `import "../styles/index.css"`,
 		expected: `import "../styles/index.css"`,
 	})
 
 	testCase("should add import without export, with proper formatting", t, {
 		code: "const test = 123;",
-		importString: `import "../styles/index.css";`,
+		statement: `import "../styles/index.css";`,
 		expected: `import "../styles/index.css";
 
 const test = 123;`,
@@ -84,7 +84,7 @@ const test = 123;`,
 		code: `import app from "app"
 
 app()`,
-		importString: `import type { Test } from "app"`,
+		statement: `import type { Test } from "app"`,
 		expected: `import app from "app"
 import type { Test } from "app"
 
@@ -93,8 +93,60 @@ app()`,
 
 	testCase("should merge type imports", t, {
 		code: `import type {Test} from "test"`,
-		importString: `import type {Test2} from "test"`,
+		statement: `import type {Test2} from "test"`,
 		expected: `import type { Test, Test2 } from "test";`,
 	})
+	t.end()
+})
+
+/**
+ * @param {string} name
+ * @param {Test} t
+ * @param {TestCase} testCase
+ */
+const requireTestCase = (name, t, { code, statement, expected }) => {
+	t.test(name, (t) => {
+		const j = withParser("tsx")
+		const root = j(code)
+		addRequire(j, root, statement)
+		t.equal(root.toSource(), expected)
+		t.end()
+	})
+}
+
+tap.test("addRequire", (t) => {
+	requireTestCase("should insert to empty file", t, {
+		code: "",
+		statement: `const test = require("test");`,
+		expected: `const test = require("test");`,
+	})
+
+	requireTestCase("should insert to file with content", t, {
+		code: "module.exports = {}",
+		statement: `const test = require("test");`,
+		// prettier-ignore
+		expected:
+`const test = require("test");
+
+module.exports = {}`,
+	})
+
+	requireTestCase("should append after existing requires", t, {
+		// prettier-ignore
+		code:
+`const thing = require("module");
+
+const abc = thing();
+module.exports = {}`,
+		statement: `const test = require("test");`,
+		// prettier-ignore
+		expected:
+`const thing = require("module");
+const test = require("test");
+
+const abc = thing();
+module.exports = {}`,
+	})
+
 	t.end()
 })
