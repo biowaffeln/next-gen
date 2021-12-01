d9057c79e14186ec6b230e67cec22d9f57ae4eb4
Mark Kvetny <m.kvetny@outlook.com>
2021-12-01 20:26:17 +0100

    add template

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div>Hello from Next.js</div>;
+};
+
+export default Home;
